94ddde5ba0d3ca7b214c228404230166678758d2
JAMESDONNELLAN-STUDE <G0043461@atu.ie>
2024-10-02 16:55:23 +0100

    Added new method called 'add'

--- src/main/java/ie/atu/week3/Calc.java
+++ src/main/java/ie/atu/week3/Calc.java
@@ -1,7 +1,27 @@
 package ie.atu.week3;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
+    // Methods //
+        System.out.println("Please enter the fist number: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Please enter the second number: ");
+        int b = sc.nextInt();
+        System.out.println("Please enter an operation: ");
+        String op = sc.next();
+        switch(op) {
+            case "add": add(a, b); break;
+            default: System.out.println("Invalid operation"); break;
+        }
 
     }
+    public static void add(int a, int b)
+    {
+        int sum = a + b;
+        System.out.println("The sum is: " + sum);
+    }
+
 }
